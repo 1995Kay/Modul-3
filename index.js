46c0f312b92a5eeb823a0d533c32a07266676215
1995Kay <kay-hansen@outlook.de>
2026-08-18 14:46:31 +0200

    Red Button On and Off

--- index.js
+++ index.js
@@ -10,4 +10,16 @@ function deIncreasedcoun() {
 function countAnzeigen() {
   var element = document.getElementById("text");
   element.innerHTML = count;
+
+  if (count < -1) {
+    alert("Der Wert ist kleiner als -1");
+  } else if (count === -1) {
+    alert("Der Wert ist gleich -1");
+  } else if (count > -1 && count < 3) {
+    alert("Der Wert zwichen  -1 und 3");
+  } else if (count === 3) {
+    alert("Der Wert ist gleich 3");
+  } else if (count > 3) {
+    alert("Der Wert ist groser als 3");
+  }
 }
