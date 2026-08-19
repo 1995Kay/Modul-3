7dcc2c3923f1b8619110900d45b42c317db91649
1995Kay <kay-hansen@outlook.de>
2026-08-19 13:13:32 +0200

    Upper and  LowerLimit add

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 var count = 0;
-function Increasedcount() {
-  count = count - 100;
-  countAnzeigen();
-}
+const upperLimit = 1000;
+const lowerLimit = -100;
 function deIncreasedcount() {
-  count = count + 100;
-  countAnzeigen();
+  if (count > lowerLimit) {
+    count = count - 100;
+    countAnzeigen();
+  } else alert("Sie können  maximal 1000 dollar einzahlen");
+}
+function Increasedcount() {
+  if (count < upperLimit) {
+    count = count + 100;
+    countAnzeigen();
+  } else alert("Sie können  maximal 1000 dollar einzahlen");
 }
 function countAnzeigen() {
   var element = document.getElementById("anzeige");
   element.innerHTML = count;
-
-  if (count === -100) {
-    alert("Sie können nur maximal 100 dollar ins minus");
-  } else if (count === 1000) {
-    alert("Sie können  maximal 1000 dollar einzahlen");
-  }
 }
