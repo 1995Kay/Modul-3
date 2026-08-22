9b2921c41fe4422aa81d67693bc624a109a6c7a4
1995Kay <kay-hansen@outlook.de>
2026-08-22 11:55:15 +0200

    Add remove&delete Button for Orderlist

--- index.js
+++ index.js
@@ -5,13 +5,22 @@ function addItemToOrder(text, price) {
   console.log(price, total, text);
   updataTotalValue();
   const ordersItemsElement = document.getElementById("orders-item");
-  ordersItemsElement.innerHTML =
-    ordersItemsElement.innerHTML + `<p>${text}: ${price}€</p>`;
+
+  ordersItemsElement.innerHTML += `
+  <div class="cart-item-row">
+    <span>${text}: ${price}€</span>
+    <button class="delete-btn" onclick="removeItem(this, ${price})">❌</button>
+  </div>`;
 }
 
 function updataTotalValue() {
   const totalValueElement = document.getElementById("total-value");
-  totalValueElement.innerHTML = total + " €";
+
+  if (total < 0.01) {
+    total = 0;
+  }
+
+  totalValueElement.innerHTML = total.toFixed(2).replace(".", ",") + " €";
 }
 function reset() {
   total = 0;
@@ -33,4 +42,11 @@ function checkoutdelvery() {
     alert(`Bestellung in Höhe von ${total}€ für die Lieferung aufgegeben `);
     reset();
   }
+}
+function removeItem(element, price) {
+  total -= price;
+
+  updataTotalValue();
+
+  element.parentElement.remove();
 }
